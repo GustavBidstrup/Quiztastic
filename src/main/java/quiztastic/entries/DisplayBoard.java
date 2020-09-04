23f1a4aac3f99c4ced5011fd64a6be92355d6cfd
GustavBidstrup <60692172+GustavBidstrup@users.noreply.github.com>
2020-09-04 10:49:30 +0200

    rette

--- src/main/java/quiztastic/entries/DisplayBoard.java
+++ src/main/java/quiztastic/entries/DisplayBoard.java
@@ -5,6 +5,7 @@ import quiztastic.app.Quiztastic;
 public class DisplayBoard {
     public static void main(String[] args) {
         Quiztastic quiz = Quiztastic.getInstance();
+
         System.out.println(quiz.getBoard());
     }
 }
