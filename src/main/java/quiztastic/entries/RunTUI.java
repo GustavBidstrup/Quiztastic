ccdd2400e8567b3e67abfb2431c4a6d1be2f9b0f
jensgelbek <jensgelbek@gmail.com>
2020-09-09 14:12:33 +0200

    socket

--- src/main/java/quiztastic/entries/RunTUI.java
+++ src/main/java/quiztastic/entries/RunTUI.java
@@ -2,11 +2,23 @@ package quiztastic.entries;
 
 import quiztastic.ui.Protocol;
 
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class RunTUI {
-    public static void main(String[] args) {
-        new Protocol(new Scanner(System.in), new PrintWriter(System.out)).run();
+    private final Socket socket;
+
+    public RunTUI(Socket socket) {
+        this.socket = socket;
+    }
+
+    public static void main(String[] args) throws IOException {
+        final int port = 6060;
+        final ServerSocket serverSocket = new ServerSocket(port);
+        Socket socket = serverSocket.accept();
+        new Protocol(new Scanner(socket.getInputStream()), new PrintWriter(socket.getOutputStream())).run();
     }
 }
